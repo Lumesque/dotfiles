978d3788fa1df7b818f9dfccdba8bda49ef2542e
Lumesque <luckiejoshu@gmail.com>
2024-02-11 05:32:46 -0500

    Adding in removal of bug for not being able to l key

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -1,7 +1,7 @@
 -- [[ KEY SETUP ]]
 local map = vim.api.nvim_set_keymap
 
-map('n', 'l', [[:IndentLinesToggle]], {})
+-- map('n', 'l', [[:IndentLinesToggle]], {})
 map('n', 't', [[:TagbarToggle]], {})
 map('n', 'ff', [[:Telescope find_files]], {})
 
