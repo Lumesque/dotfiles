e24129662963060e761885fe9ff8bb9e98a57fc6
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-08-10 18:20:43 -0400

    Adding in abbreviations and such

--- shell_setups/fish/.config/fish/conf.d/abbreviations.fish
+++ shell_setups/fish/.config/fish/conf.d/abbreviations.fish
@@ -1,3 +1,8 @@
+function multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
 
 # Basically an alias file
 abbr --add ls ls -aF --color=always
+abbr --add ggpush git push origin $(git branch --show-current)
+abbr --add dotdot --regex '^\.\.+$' --function multicd
