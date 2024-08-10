01c11e312162afd70fb826a87bf3e4ca618559e6
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-08-10 18:05:18 -0400

    Adding in rest of fish configuration so that stow can properly softlink directories

--- shell_setups/fish/.config/fish/functions/up.fish
+++ shell_setups/fish/.config/fish/functions/up.fish
@@ -1,7 +1,7 @@
 function up
-set -l this $argv[1]
-while test $this -gt 0;
-cd ..
-set -f this (math $this - 1)
-end
+    set -l this $argv[1]
+    while test $this -gt 0;
+        cd ..
+        set -f this (math $this - 1)
+    end
 end
