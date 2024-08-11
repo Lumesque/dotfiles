c6c5906d08dcdcc1d7cc6d33b3e114c6bdb58e37
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-08-11 13:00:06 -0400

    Updating git functions, testing out other ways to write up

--- shell_setups/fish/.config/fish/conf.d/paths.fish
+++ shell_setups/fish/.config/fish/conf.d/paths.fish
@@ -1,5 +1,5 @@
-add_to_path /home/lumesque/.local/bin
-add_to_path /home/lumesque/bin
-add_to_path /home/lumesque/.local/share/gem/ruby/3.0.0/bin
+fish_add_path /home/lumesque/.local/bin
+fish_add_path /home/lumesque/bin
+fish_add_path /home/lumesque/.local/share/gem/ruby/3.0.0/bin
 # add_to_path /home/lumesque/.local/bin/zig-linux-x86_64-0.13.0-dev.340+d750a78b2
-source_path ~/.cargo/env
+# source ~/.cargo/env
--- shell_setups/fish/.config/fish/functions/up.fish
+++ shell_setups/fish/.config/fish/functions/up.fish
@@ -1,7 +1,12 @@
 function up
     set -l this $argv[1]
-    while test $this -gt 0;
-        cd ..
-        set -f this (math $this - 1)
+    set -l result (string match --regex '\D*' $this)
+    if not test -z $result;
+        echo "Argument provided must be a number" >&2
+        return 2
+    else if test $this -lt 0;
+        echo "Argument provided cannot be less than 0" >&2
+        return 1
     end
+    cd (string repeat -n $this ../)
 end
